aa37f68d5d9d6e7709ee5956299f23612e57725c
Yoshiko Tsuka <37019354+yoshiko-tsuka@users.noreply.github.com>
2023-11-10 17:02:56 +1100

    lesson16 how to use vite

---
+++ lesson16_vite/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Message from "./components/Message.vue";
+</script>
+
+<template>
+  <div>
+    <Message>Hello World</Message>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ lesson16_vite/src/components/Message.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="box">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  background: #e3e3e3;
+  padding: 10px;
+  border: 1px solid #c5c5c5;
+}
+</style>
